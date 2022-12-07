e298127cc698a2e1e5f1256adfa5773d6b3d92bd
Danila <taradash98@gmail.com>
2022-12-07 22:55:25 +0300

    HW

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+int n =Convert.ToInt32(Console.ReadLine());
+
+int n1 = n / 10;
+int n2 = n1 % 10;
+Console.WriteLine(n2);
---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n =Convert.ToInt32(Console.ReadLine());
+
+if (n % 100 != 0)
+Console.WriteLine("Нет Третьей цифры");
+else
+
+int n1 = n / 10;
+int n2 = n1 % 10;
+Console.WriteLine(n2);

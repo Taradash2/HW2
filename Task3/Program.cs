7cd023a8c9856fa71ccdb0688a9ecaa304674152
Danila <taradash98@gmail.com>
2022-12-07 22:51:29 +0300

    Task1 & Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.Write("Введите число дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+Console.Write("Вы ошиблись!\nВведите число дня недели: ");
+n = Convert.ToInt32(Console.ReadLine());
+}
+
+/*if (n == 1)
+Console.WriteLine("нет");
+else if (n == 2)
+Console.WriteLine("нет");
+else if (n == 3)
+Console.WriteLine("нет");
+else if (n == 4)
+Console.WriteLine("нет");
+else if (n == 5)
+Console.WriteLine("нет");
+else if (n == 6)
+Console.WriteLine("да");
+else
+Console.WriteLine("да");*/
+
+if (n <= 7 && n > 5) 
+Console.WriteLine("Yes");
+else
+Console.WriteLine("no");
